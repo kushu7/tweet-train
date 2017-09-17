4b705010028d3de9602aaa86cc7ecfa0aa36bcc7
kushu7 <31707153+kushu7@users.noreply.github.com>
2017-09-18 01:12:48 +0530

    posting tweet method

--- Code/Server/bot.js
+++ Code/Server/bot.js
@@ -1,4 +1,19 @@
 var twit = require('twit');
 var keys = require('./keys'); //Configuration keys
 
-var t = new Twit(keys);
+var T = new twit(keys);
+
+var status = {status: 'byebye'};
+
+//setInterval(tweetIt,1000*20);
+//For Posting Client Tweet
+function tweetIt()
+{
+	T.post('statuses/update', status,
+function(err, data, response){
+	  if(err)console.log(err);
+	   var PostId = data.id;
+		console.log('Posted' + 'Post id:' + PostId);
+});
+}
+
--- Code/Server/keys.js
+++ Code/Server/keys.js
@@ -1,9 +1,7 @@
 module.exports = {
-{
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
+  consumer_key:         'rW6qlDclrN6NcHOagfo7KusqA',
+  consumer_secret:      'bkprRlY9qRen0NWbOMIabmZlLlf6VlDFogn1BA7pXxjlBgbwaV',
+  access_token:         '909482718450159616-ukddszSQt720oeUt7nRhrc4buqPoGzi',
+  access_token_secret:  'kMjWxlSRgNDG0jhkDj3Dzy0bjGN6J6vd6kJ4RLWXh9CNp',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-}
 }
