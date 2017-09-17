b8ea59d0a3c97a1fe0e952d0aa83a8e58c6ffb14
kushu7 <31707153+kushu7@users.noreply.github.com>
2017-09-18 02:00:25 +0530

    new methods

--- Code/Server/bot.js
+++ Code/Server/bot.js
@@ -4,16 +4,35 @@ var keys = require('./keys'); //Configuration keys
 var T = new twit(keys);
 
 var status = {status: 'byebye'};
-
+var cunt = 5;
+var searchQuery = '#paytm'; //query for searching tweets
+var query = {q : searchQuery, count: cunt};
 //setInterval(tweetIt,1000*20);
+
+
+
+
+//For searching Tweet with query
+function getTweets(query,cunt)
+{
+	T.get('search/tweets',query, function(err, data, response) {
+		if(err){}
+		else{
+	  for(var i =0;i<data.statuses.length;i++)
+		console.log(data.statuses[i].text +'\n');
+
+	}
+	});
+}
+
 //For Posting Client Tweet
 function tweetIt()
 {
-	T.post('statuses/update', status,
-function(err, data, response){
-	  if(err)console.log(err);
-	   var PostId = data.id;
+		T.post('statuses/update', status,
+		function(err, data, response){
+		  if(err)console.log(err);
+		  var PostId = data.id;
 		console.log('Posted' + 'Post id:' + PostId);
-});
+	});
 }
 
