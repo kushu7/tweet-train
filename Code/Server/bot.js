e3e16c1d3961f5a194c20901db3cf9937f3a4a45
kushu7 <31707153+kushu7@users.noreply.github.com>
2017-09-18 16:27:46 +0530

    working

--- Code/Server/bot.js
+++ Code/Server/bot.js
@@ -1,28 +1,59 @@
 var twit = require('twit');
 var keys = require('./keys'); //Configuration keys
-
+var express = require('express');
+var fs = require('fs');
+var url = require('url');
+var app = express();
 var T = new twit(keys);
-
-var status = {status: 'byebye'};
-var cunt = 5;
 var searchQuery = '#paytm'; //query for searching tweets
+var cunt = 1;
 var query = {q : searchQuery, count: cunt};
-//setInterval(tweetIt,1000*20);
+
+app.get('/search',function(req,res)
+{
+	var query = url.parse(req.url,true).query;
+	searchQuery =query.id;
+	if(query.count !== undefined)
+	cunt = query.count;
+	console.log('query:' + searchQuery + ', count:' + cunt);
+	query = {q : searchQuery, count: cunt};
+	getTweets(query,cunt,res);
+	
+	console.log(new Date().toLocaleString() + ': Done ');
+});
+//Server
+var server = app.listen(3000,'0.0.0.0',function(){
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Listening on ' + host + ":" + port);
+});
+var status = {status: 'byebye'};
+
+
 
 
 
 
 //For searching Tweet with query
-function getTweets(query,cunt)
+function getTweets(query,cunt,res)
 {
+	var obj="";
 	T.get('search/tweets',query, function(err, data, response) {
-		if(err){}
+		if(err){
+			savelog(err,'getTweets');
+			res.end('Something went Wrong');
+		}
 		else{
-	  for(var i =0;i<data.statuses.length;i++)
-		console.log(data.statuses[i].text +'\n');
-
+		for(var i =0;i<data.statuses.length;i++)
+		{
+		obj += data.statuses[i].text +'<br><br>';
+		}
+		res.send(obj);
+		res.end('Executed');
+		
 	}
 	});
+	
 }
 
 //For Posting Client Tweet
@@ -30,9 +61,22 @@ function tweetIt()
 {
 		T.post('statuses/update', status,
 		function(err, data, response){
-		  if(err)console.log(err);
+		  if(err)savelog(err,'tweetIt');
 		  var PostId = data.id;
 		console.log('Posted' + 'Post id:' + PostId);
 	});
+}
+
+//Saving logs 
+function savelog(err,method)
+{
+	var time = new Date().toLocaleString();
+	fs.appendFileSync('logs.txt', time + ': Method= ' + method+ 'Error= ' + err,
+	function(err)
+	{
+		if(err){}
+		console.log('log saved');
+	});
+
 }
 
