ad9efd4ed4d142693366cb45a3fb92db6192584a
kushu7 <31707153+kushu7@users.noreply.github.com>
2017-09-19 16:37:05 +0530

    server done

--- Code/Server/bot.js
+++ Code/Server/bot.js
@@ -7,16 +7,18 @@ var path= require('path');
 var os = require('os'); 
 var Twitter = require('twitter-oauth-agent');
 var request = require('request');
-
-
+var querystring = require('querystring');
+var assign = require('object-assign');
+var stringify = require('json-stringify');
 //**************Usable variables*********************//
 var app = express();
-var T = new twit(keys); //Keys of Clients
+//var T = new twit(keys); //Keys of Clients
 var searchQuery = '#paytm'; //query for searching tweets
 var tweet="";
 var status = {status: tweet}; //tweeting
 var cunt = 1;
 var query = {q : searchQuery, count: cunt};
+var call = 'http://127.0.0.1:3000/twitter/callback';
 //*************SERVER*********************
 var server = app.listen(3000,'0.0.0.0',function(){ //0.0.0.0 Listening to machine address
 	var host = server.address().address;
@@ -24,51 +26,48 @@ var server = app.listen(3000,'0.0.0.0',function(){ //0.0.0.0 Listening to machin
 	console.log('Listening on ' + host + ":" + port);
 });
 
+
  
 // Get the token and send to the client 
 app.get('/',function(req,res)
 {
-Twitter({
-  consumer_key: 'rW6qlDclrN6NcHOagfo7KusqA',
-  consumer_secret: 'bkprRlY9qRen0NWbOMIabmZlLlf6VlDFogn1BA7pXxjlBgbwaV',
-  callback: 'http://127.0.0.1:3000/twitter/callback'
-}, function(err, token) {
+	console.log('in twiiter oauth');
+Twitter(keys, function(err, token) {
   if (err) return res.status(500).send({ error: err.message });
-  var link = 'https://api.twitter.com/oauth/authenticate?oauth_token='+token.oauth_token+'&prompt=consent';
-  res.writeHead(301,{Location:link});
- 
-console.log('redirect');  res.end();
+ client_login(token,res);  });
 });
-});
-
+//client login
+function client_login(token,res)
+{
+obj = assign(token);
+	console.log('in client');
+	var endpoint = 'https://api.twitter.com/oauth/authenticate';
+	var url = endpoint + '?' + querystring.stringify(obj);
+	res.redirect(url);
+	
+  // console.log(data);
+  }
+	
 app.get('/twitter/callback',function(req,res)
 { 
-var query = url.parse(req.url,true).query;
-var oauth_verifier = oauth_verifier;
-var oauth_token = query.oauth_token;
-getKeys(oauth_verifier,oauth_token);	
+var token = url.parse(req.url,true).query;
+console.log('in callback');
+getKeys(token,res);	
 });
 
- function getKeys(oauth_verifier,oauth_token){
-            var clientServerOptions = {
-                uri: 'https://api.twitter.com/oauth/access_token',
-                method: 'POST',
-                headers: {
-					Authorization: OAuth oauth_consumer_key="cChZNFj6T5R0TigYB9yd1w",
-                     oauth_nonce="a9900fe68e2573b27a37f10fbad6a755",
-                     oauth_signature="39cipBtIOHEEnybAR4sATQTpl2I%3D",
-                     oauth_signature_method="HMAC-SHA1",
-                     oauth_timestamp="1318467427",
-                     oauth_token="NPcudxy0yU5T3tBzho7iCotZ3cnetKwcTIRlX0iwRl0",
-                     oauth_version="1.0",
-					
-                    'Content-Type': 'Content-Type: application/x-www-form-urlencoded'
-                }
-            }
-            request(clientServerOptions, function (error, response) {
-                console.log(error,response.body);
-                return;
-            });
+
+//access token 
+ function getKeys(code,res){
+            Twitter({
+  consumer_secret: keys.consumer_secret,
+  oauth_verifier:code.oauth_verifier,
+  consumer_key: keys.consumer_key,
+  oauth_token: code.oauth_token
+}, function(err, profile) {
+	var a= stringify(profile);
+	console.log('got tokens');
+  res.send(a);
+});
         }
 		
 		
--- Code/Server/keys.js
+++ Code/Server/keys.js
@@ -1,7 +1,8 @@
 module.exports = {
   consumer_key:         'rW6qlDclrN6NcHOagfo7KusqA',
   consumer_secret:      'bkprRlY9qRen0NWbOMIabmZlLlf6VlDFogn1BA7pXxjlBgbwaV',
-  access_token:         '909482718450159616-ukddszSQt720oeUt7nRhrc4buqPoGzi',
-  access_token_secret:  'kMjWxlSRgNDG0jhkDj3Dzy0bjGN6J6vd6kJ4RLWXh9CNp',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  callback : 'http://127.0.0.1:3000/twitter/callback'
+  //access_token:         '909482718450159616-ukddszSQt720oeUt7nRhrc4buqPoGzi',
+  //access_token_secret:  'kMjWxlSRgNDG0jhkDj3Dzy0bjGN6J6vd6kJ4RLWXh9CNp',
+  //timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
